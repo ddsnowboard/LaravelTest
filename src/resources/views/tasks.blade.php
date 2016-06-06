bb0ddecd343699b6d542b16761212de2dccd4097
Will Koster <ddsnowboard@gmail.com>
2016-06-06 16:47:59 -0500

    Add some more views
    Also, remove old view. Now they at least look right, even though they
    aren't functional.

--- src/resources/views/tasks.blade.php
+++ src/resources/views/tasks.blade.php
@@ -1,5 +1,39 @@
-<html>
-<body>
-This is a test. This text should show up. I wonder if it will work
-</body>
-</html>
+@extends('layouts.app')
+
+@section('content')
+ <!-- Bootstrap Boilerplate... -->
+
+    <div class="panel-body">
+        <!-- Display Validation Errors -->
+        @include('common.errors')
+
+        <!-- New Task Form -->
+        <form action="{{ url('task') }}" method="POST" class="form-horizontal">
+            {{ csrf_field() }}
+
+            <!-- Task Name -->
+            <div class="form-group">
+                <label for="task" class="col-sm-3 control-label">Task</label>
+
+                <div class="col-sm-6">
+                    <input type="text" name="name" id="task-name" class="form-control">
+                </div>
+            </div>
+
+            <!-- Add Task Button -->
+            <div class="form-group">
+                <div class="col-sm-offset-3 col-sm-6">
+                    <button type="submit" class="btn btn-default">
+                        <i class="fa fa-plus"></i> Add Task
+                    </button>
+                </div>
+            </div>
+        </form>
+    </div>
+
+    <ul>
+    @foreach($tasks->all() as $task)
+        <li>$task->name</li>
+    @endforeach
+    </ul>
+@endsection
