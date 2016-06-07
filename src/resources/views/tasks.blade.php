377f2ce701213763a1f6e62175d1da8bb907f122
Will Koster <ddsnowboard@gmail.com>
2016-06-07 14:10:20 -0500

    Finish the template kinda
    It looks like a dog's breakfast, but it works like it's supposed to, so
    that's nice. I think I'm going to try the advanced tutorial now.

--- src/resources/views/tasks.blade.php
+++ src/resources/views/tasks.blade.php
@@ -1,39 +1,52 @@
 @extends('layouts.app')
 
 @section('content')
- <!-- Bootstrap Boilerplate... -->
+    <!-- Bootstrap Boilerplate... -->
 
     <div class="panel-body">
-        <!-- Display Validation Errors -->
-        @include('common.errors')
+    <!-- Display Validation Errors -->
+    @include('common.errors')
 
-        <!-- New Task Form -->
-        <form action="{{ url('task') }}" method="POST" class="form-horizontal">
-            {{ csrf_field() }}
+    <!-- New Task Form -->
+    <form action="{{ url('task') }}" method="POST" class="form-horizontal">
+    {{ csrf_field() }}
 
-            <!-- Task Name -->
-            <div class="form-group">
-                <label for="task" class="col-sm-3 control-label">Task</label>
+    <!-- Task Name -->
+    <div class="form-group">
+    <label for="task" class="col-sm-3 control-label">Task</label>
 
-                <div class="col-sm-6">
-                    <input type="text" name="name" id="task-name" class="form-control">
-                </div>
-            </div>
-
-            <!-- Add Task Button -->
-            <div class="form-group">
-                <div class="col-sm-offset-3 col-sm-6">
-                    <button type="submit" class="btn btn-default">
-                        <i class="fa fa-plus"></i> Add Task
-                    </button>
-                </div>
-            </div>
-        </form>
+    <div class="col-sm-6">
+    <input type="text" name="name" id="task-name" class="form-control">
+    </div>
     </div>
 
+    <!-- Add Task Button -->
+    <div class="form-group">
+    <div class="col-sm-offset-3 col-sm-6">
+    <button type="submit" class="btn btn-default">
+    <i class="fa fa-plus"></i> Add Task
+    </button>
+    </div>
+    </div>
+    </form>
     <ul>
     @foreach($tasks->all() as $task)
-        <li>$task->name</li>
+        <li>
+        <table>
+        <tr>
+        <td>{{ $task->name }}</td>
+        <td style="color: red">
+        <form action="{{ url("task") . "/" . $task->id }}" method="POST">
+        {{ csrf_field() }}
+        {{ method_field("DELETE") }}
+        <button type="submit">
+        DELETE
+        </button>
+        </td>
+        </tr>
+        </table>
+        </li>
     @endforeach
     </ul>
+    </div>
 @endsection
